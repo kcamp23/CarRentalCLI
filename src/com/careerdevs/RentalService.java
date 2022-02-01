1708531667143e663d0db515a6b5186f9b9639b7
Kat Camp <kcamp552369@gmail.com>
2022-01-31 19:07:13 -0500

    functioning but before clean up

--- src/com/careerdevs/RentalService.java
+++ src/com/careerdevs/RentalService.java
@@ -2,16 +2,25 @@ package com.careerdevs;
 
 import java.util.Scanner;
 
-import static java.util.Scanner.*;
-
 public class RentalService {
     public static void main(String[] args) {
+
+        Car[] garage = new Car[4];
+        Car car1 = new Car("Ford", "Mustang", true);
+        Car car2 = new Car("Pontiac", "Torrent", true);
+        Car car3 = new Car("Dodge", "Ram", true);
+        Car car4 = new Car("Nissan", "Stanza", true);
+        garage[0] = car1;
+        garage[1] = car2;
+        garage[2] = car3;
+        garage[3] = car4;
+
         System.out.println("Welcome to Off the Walls  Rental CLI\n\r");
-        mainMenu();
-        endProgram();
+        mainMenu(garage);
+
     }
 
-    public static void mainMenu() {
+    public static void mainMenu(Car[] garage) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Are you renting or returning today?\n\r");
         System.out.print("1) Rent\n\r");
@@ -24,36 +33,26 @@ public class RentalService {
 
         if (mainMenuOption == 1) {
             //run rental menu
-            rentACar();
+            rentACar(garage);
         } else if (mainMenuOption == 2) {
             //// run return menu
+        }else if (mainMenuOption == 3){
+            endProgram();
+        }else{
+            System.out.print("Car selection invalid, please try again\n");
+            mainMenu(garage);
         }
+
+
     }
 
     public static void endProgram() {
         System.out.print("Thank you for Choosing Off the Walls and have a great day!!");
+
     }
 
-    public static void rentACar() {
+    public static void rentACar(Car[] garage) {
         System.out.print("Which car would you like to rent today?\n");
-        availableCars();
-
-
-    }
-
-    public static void availableCars() {
-
-        Car[] garage = new Car[4];
-        Car car1 = new Car("Ford", "Mustang", true);
-        Car car2 = new Car("Pontiac", "Torrent", true);
-        Car car3 = new Car("Dodge", "Ram", true);
-        Car car4 = new Car("Nissan", "Stanza", true);
-
-        garage[0] = car1;
-        garage[1] = car2;
-        garage[2] = car3;
-        garage[3] = car4;
-
 
         for (int i = 0; i < garage.length; i++) {
             if (garage[i].isRented() == true) {
@@ -70,45 +69,71 @@ public class RentalService {
         System.out.print(" You have choosen the  " + garage[(rentalOption - 1)].getName() + "\n");
 
         garage[(rentalOption - 1)].setRented(false);
+                whatsNext(garage);
 
-        System.out.print("The following cars are now available: \n");
-        for (int i = 0; i < garage.length; i++) {
-            if (garage[i].isRented() == true) {
+//        System.out.print("The following cars are now available: \n");
+//        for (int i = 0; i < garage.length; i++) {
+//            if (garage[i].isRented() == true) {
+//
+//                System.out.print((i + 1) + ")  " + garage[i].getName() + "\n");
+//            } else {
+//                 System.out.print((i + 1) + " ) " + "Unavailable \n");
+//            }
 
-                System.out.print((i + 1) + ")  " + garage[i].getName() + "\n");
-            } else {
-                // System.out.print((i + 1) + " ) " + "Unavailable \n");
-            }
-            System.out.println("What would you like to do next?");
+    }
+    public static  void whatsNext(Car[] garage){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What would you like to do next?");
             System.out.println("1) Rent another car?");
-            System.out.print("2) Return a car");
-            System.out.println("Return to main menu");
+            System.out.println("2) Return a car");
+            System.out.println("3) Return to main menu");
             int rentalNext = scanner.nextInt();
 
             switch (rentalNext) {
                 case 1:
-                    rentACar();
+                    rentACar(garage);
                     break;
 
                 case 2:
-                    //returnaCar();
+                    returnACar(garage);
                     break;
 
                 case 3:
-                    mainMenu();
+                    mainMenu(garage);
+                    break;
+
+                default:
+                    mainMenu(garage);
+                    break;
 
 
             }
         }
 
 
-    }
 
 
-    public static void returnACAr(){
 
+    public static void returnACar(Car[] garage){
 
-        System.out.print("What car are you returning today? \n");
+        System.out.print("Which car would you like to rent today?\n");
+
+        for (int i = 0; i < garage.length; i++) {
+            if (garage[i].isRented() == false) {
+
+                System.out.print((i + 1) + ")  " + garage[i].getName() + "\n");
+            } else  {
+                // System.out.print((i + 1) + " ) " + "Unavailable \n");
+            }
+
+        }
+        System.out.println("Which car  would you like to return ?");
+        Scanner scanner = new Scanner(System.in);
+        int rentalOption = scanner.nextInt();
+        System.out.print(" You have choosen the  " + garage[(rentalOption - 1)].getName() + "\n");
+
+        garage[(rentalOption - 1)].setRented(true);
+        whatsNext(garage);
     }
 }
 
